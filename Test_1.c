eb5259052ba470fd7af9575aa0319cc77b4ffc5d
Egor <120785170+SCHVRevan@users.noreply.github.com>
2025-03-12 17:30:06 +0700

    Update Test_1.c

--- Test_1.c
+++ Test_1.c
@@ -1,4 +1,5 @@
-// компилируется с использованием mpicxx для поддержки библиотеки math.h
+// компилируется:
+// mpicxx -O Test_1.c -o Test_1 
 #include <stdio.h>
 #include <math.h>
 #include <mpi.h>
@@ -7,20 +8,24 @@
 int main(int argc, char **argv) {
    // Объявляю переменные и выделяю память под массив вещественных чисел
    int rank, size;
-   double x, sum, buff, y[5];
+   double x, sum, buff;
+   double y[5];
    srand(time(NULL));
    MPI_Status status;
    MPI_Init(&argc, &argv);
    MPI_Comm_size(MPI_COMM_WORLD, &size);
    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
    // Для каждой копии процесса инициализирую значение x и y
-   // Приведение типов необходимо для получения вещественного результата
    x = cos((rank + 1) * (double)rand() / RAND_MAX);
+   //printf("Process: %d  X: %f\n", rank, x);
    for (int k = 1; k <= 5; k++) {
 	y[k-1] = sin(k*x) / (double)k;
-	// Сразу же проверяю отрицательность элемента и считаю локальную сумму
+	// Сразу проверяю отрицательность элемента и считаю локальную сумму
 	if (y[k-1] < 0) sum += y[k-1];
    }
+   for (int i = 0; i < 5; i++) printf("Process: %d   y%d = %f\n", rank, i, y[i]);
+   // Проверка локальных сумм
+   printf("Process %d:  Sum: %f\n", rank, sum);
    // Отправляю локальные суммы процессу с номером 1
    if (rank != 1) MPI_Send(&sum, 1, MPI_DOUBLE, 1, 5, MPI_COMM_WORLD);
    else {
@@ -29,7 +34,7 @@ int main(int argc, char **argv) {
 		MPI_Recv(&buff, 1, MPI_DOUBLE, i % size, 5, MPI_COMM_WORLD, &status);
 		sum += buff;
 	}
-	printf("Process %d:  Sum: %f\n", rank, sum);
+	printf("Process %d:  Sum: %f\n\n", rank, sum);
    }
    MPI_Finalize();
    return 0;
