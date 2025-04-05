b26d56d0eece46ae49520a77b52336894477c5c7
Egor <120785170+SCHVRevan@users.noreply.github.com>
2025-04-05 18:31:39 +0700

    Update test_5.c

--- test_5.c
+++ test_5.c
@@ -11,13 +11,18 @@ using namespace std;
 // Required function
 long double f(long double x, long double y) {
 	long double res = 1;
-	if (abs(x) + abs(y) <= 1) res = abs(x * y * (x + y));
+	// abs() работает некорректно
+	if (x < 0) x *= -1;
+	if (y < 0) y *= -1;
+	// Выполняется проверка на принадлежность координат области
+	// Проверка x + y <= 1 не срабатывает корректно
+	if (x + y <= 1.000000000001) res = abs(x * y * (x + y));
 	return res;
 }
 
 int main(int argc, char **argv) {
    int size, rank, tag = 21;
-   long double clock, buff, hx, hy, x, y, res = 0, s = 1, x0 = -1, xn = 1, y0 = -1, yn = 1, N = 100000;
+   long double clock, hx, hy, x, y, buff = 0, res = 0, s = 1, x0 = -1, xn = 1, y0 = -1, yn = 1, N = 100000;
    MPI_Status status;
    MPI_Init(&argc, &argv);
    MPI_Comm_size(MPI_COMM_WORLD, &size);
@@ -27,25 +32,29 @@ int main(int argc, char **argv) {
    hy = (yn - y0) / N;
    s = hx * hy;
 
-   // Start timer on process 0
+   // Старт таймера на процессе с рангом 0
    if (rank == 0) clock = MPI_Wtime();
 
-   for (int i = 0 + rank; i < N; i += size) {
+   // Распределение столбцов сетки по процессам
+   for (int i = 1 + rank; i < N; i += size) {
 	x = x0 + i*hx + hx/2;
+	// Вычисление интеграла по столбцу
 	for (int j = 0; j < N; j++) {
 		y = y0 + j*hy + hy/2;
 		buff = f(x, y);
-		if (buff < 1) res += buff * s;
+		// Проверка принадлежности координат области
+		if (buff < 1) res += buff;
 	}
    }
 
-   // Sending all local results to process 0
+   // Сбор всех локальных результатов и формирование итогового значения процессом с рангом 0
    if (rank != 0) MPI_Send(&res, 1, MPI_LONG_DOUBLE, 0, tag, MPI_COMM_WORLD);
    else {
 	for (int i = 1; i < size; i++) {
 		MPI_Recv(&buff, 1, MPI_LONG_DOUBLE, i, tag, MPI_COMM_WORLD, &status);
 		res += buff;
 	}
+	res *= s;
    }
 
    // Constructing result on process 0
