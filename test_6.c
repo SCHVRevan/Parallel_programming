91c7c6448c81416e7117c0696ce0edd8c30c094d
Egor <120785170+SCHVRevan@users.noreply.github.com>
2025-04-25 09:20:23 +0700

    Update test_6.c

--- test_6.c
+++ test_6.c
@@ -1,9 +1,6 @@
 // compile:
 // g++ -fopenmp test_6.c -o test_6
 
-// 4. f(x, y) = | xy(x + y)|, D = {|x| + |y| <= 1}
-// Метод ячеек: sum(i, N)(f(x[i], y[i])*S[i])
-
 #include <stdio.h>
 #include <math.h>
 #include <iostream>
@@ -12,7 +9,7 @@
 using namespace std;
 
 // Required function
-long double f(long double x, long double y) {
+long double f(long double &x, long double &y) {
 	long double res = 0;
 	if (x < 0) x *= -1;
 	if (y < 0) y *= -1;
@@ -22,8 +19,8 @@ long double f(long double x, long double y) {
 	return res;
 }
 
-int main() {
-   int N = 100000;
+int main(int argc, char *argv[]) {
+   int N = 60000;
    double clock;
    long double hx, hy, x, y, buff = 0, res = 0, s = 1, x0 = -1, xn = 1, y0 = -1, yn = 1;
 
@@ -34,15 +31,19 @@ int main() {
    // Старт таймера на процессе с рангом 0
    clock = omp_get_wtime();
 
-   omp_set_num_threads(12);
+   omp_set_num_threads(atoi(argv[1]));
    #pragma omp parallel private(x, y)
    {
-	#pragma omp for schedule(static) collapse(1) reduction(+ : res)
+	#pragma omp single
+	{
+		cout << "Threads num: " << omp_get_num_threads() << "\n";
+	}
+	#pragma omp for schedule(guided) collapse(2) reduction(+ : res)
 	// Распределение столбцов сетки по процессам
 	for (int i = 0; i < N; i++) {
-		x = x0 + i*hx + hx/2;
 		// Вычисление интеграла по столбцу
 		for (int j = 0; j < N; j++) {
+			x = x0 + i*hx + hx/2;
 			y = y0 + j*hy + hy/2;
 			res += f(x, y);
 		}
